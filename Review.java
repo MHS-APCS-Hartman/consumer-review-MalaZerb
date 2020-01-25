ae29b349dee6141d81e1f3cec992fd1ff74a5789
MalaZerb <59923720+MalaZerb@users.noreply.github.com>
2020-01-25 12:09:09 -0500

    Update Review.java

--- Review.java
+++ Review.java
@@ -165,30 +165,31 @@ public class Review {
       return randomNegativeAdj();
     }
   }
-   public static int starRating(String fileName)
+    public static int starRating(String fileName)
    {
    
    //get total sentiment 
-   //if sentiment val is 
+   
    int totalSentiment = (int) totalSentiment(fileName);
   
-   if(totalSentiment < 0) //Reviews with multiple negative words like simple review would return either a sentiment value of 0 or lower
+   if(totalSentiment < 0) //Reviews with multiple negative words like simple review would return either a value of 0 or lower
    {
      return 1;
    }
-   else if(totalSentiment < 5) //Reviews with a balanced amount of negative and positive words like 26West Review would have sentiment  values greater than 0, but less than 5
+   else if(totalSentiment < 5) //Reviews with a balanced amount of negative and positive words like 26West Review would have values 
    {
     return 2; 
    }
    
-   else if(totalSentiment < 15 ) //Reviews with more positive words than neagtive words had total sentiment greater than 5 but less than 15
+   else if(totalSentiment < 15 ) //Any review that has more positive words has a 3 star rating
    {
     return 3;
    }
    else
    {
-     return 4; //Review with mostly positive words had a sentimental value of 20 or higher
+     return 4; 
    }
  }
+
 
 }
